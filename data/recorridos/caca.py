11a01fd8fbe36f1cd810098a3d8e5bf6a8d397e2
athinasalimm <127424862+athinasalimm@users.noreply.github.com>
2025-06-06 11:48:04 -0300

    caca_catu

---
+++ data/recorridos/caca.py
@@ -0,0 +1,12 @@
+import pandas as pd
+trips2020 = pd.read_csv('data/recorridos/trips_2020.csv')
+trips2021 = pd.read_csv('data/recorridos/trips_2021.csv')
+trips2022 = pd.read_csv('data/recorridos/trips_2022.csv')
+trips2023 = pd.read_csv('data/recorridos/trips_2023.csv')
+trips2024 = pd.read_csv('data/recorridos/trips_2024.csv')
+
+print(trips2020.shape)
+print(trips2021.shape)
+print(trips2022.shape)
+print(trips2023.shape)  
+print(trips2024.shape)
